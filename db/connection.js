b8b5f94cad376b22ae2d71342a42c3263c88e6d9
Gemma Baker <gemmabaker@macbookpro.lan>
2025-05-05 21:23:47 +0100

    updated settings for hosting

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,8 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const connection = new Pool({
-  database: process.env.PG_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  max: 2,
 });
 
 module.exports = connection;
